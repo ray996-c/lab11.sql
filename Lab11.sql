354d75555c25b1cabbbe895c22fed8798d276902
ray996-c <101057667@georgebrown.ca>
2024-11-26 10:38:29 -0500

    Update Lab11.sql

--- Lab11.sql
+++ Lab11.sql
@@ -1,25 +1,18 @@
-SELECT * FROM courses;
-
-SELECT * FROM assignments
-LIMIT 10; 
-
-SELECT count(*) FROM courses;
-
-SELECT min(due_date) FROM assignments;
-
-SELECT *
-FROM  courses
-WHERE course_name LIKE 'Intro%';
-
-SELECT *
-FROM  assignments
-WHERE status != 'Completed'
-ORDER BY due_date;
-
--- Use this query as a reference for the next step
-SELECT course_id, title, status, due_date
+SELECT * FROM assignments;
+SELECT title, due_date
 FROM assignments
-WHERE status != 'Completed'	
-  AND course_id LIKE 'COMM%'
-  AND due_date < '2024-12-31'
-ORDER BY due_date;
+WHERE course_id = 'COMP1234';
+SELECT min(due_date) AS earliest_due_date
+FROM assignments;
+SELECT max(due_date) AS latest_due_date
+FROM assignments;
+SELECT title, course_id
+FROM assignments
+WHERE due_date = '2024-10-08';
+SELECT title, due_date
+FROM assignments
+WHERE due_date LIKE '2024-10%';
+SELECT max(due_date) AS most_recent_completed_due_date
+FROM assignments
+WHERE status = 'Completed';
+
